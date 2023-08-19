15e17b6247b1c8420d7edfabce034bb2bda0cc1f
Ena <j.wickes999@gmail.com>
2023-08-19 14:06:23 +0100

    Initial Commit

---
+++ BloomRight/BloomRightApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomRightApp.swift
+//  BloomRight
+//
+//  Created by Ena Ashley on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BloomRightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
