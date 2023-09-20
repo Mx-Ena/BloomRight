ae95b358ce37cca21d8d2201eb9ac5616f5eb2af
Ena <j.wickes999@gmail.com>
2023-09-20 17:42:09 +0100

    Added new plant photos and created new views.

---
+++ BloomRight/BloomRightView.swift
@@ -0,0 +1,8 @@
+//
+//  BloomRightView.swift
+//  BloomRight
+//
+//  Created by Ena Ashley on 28/08/2023.
+//
+
+import Foundation
---
+++ BloomRight/PlantBoxView.swift
@@ -0,0 +1,8 @@
+//
+//  PlantBoxView.swift
+//  BloomRight
+//
+//  Created by Ena Ashley on 28/08/2023.
+//
+
+import Foundation
